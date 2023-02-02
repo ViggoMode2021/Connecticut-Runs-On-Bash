a81602b6a60b6e15794e8445880b32f55ad168f7
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-02-02 09:36:07 -0500

    Update backup-to-aws.sh

--- backup-to-aws.sh
+++ backup-to-aws.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
-mkdir -p -- "$script-backuos"
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+file="/path/to/accessKeys.csv" # CHANGE WITH ACCESS KEYS
+cnt=0
+ export AWS_DEFAULT_REGION=us-east-1
+ export AWS_DEFAULT_OUTPUT=text
+while IFS=, read var1 var2  
+do 
 
-tar -zcf usr/local/bin/script-backups/backup-$(date +%Y%m%d).tar.gz -C usr/local/ bin
+      if [ $cnt = 1 ]
+      then 
+        echo $cnt
+        export AWS_ACCESS_KEY_ID=$var1
+        export AWS_SECRET_ACCESS_KEY=$var2
+      fi
+
+      cnt=$(($cnt + 1))
+
+done < <(tr -d '\r' < "${file}" )
+
+mkdir -p -- "$script-backups"
+
+tar -zcf usr/local/bin/script-backups/script-backup-$(date +%Y%m%d).tar.gz -C usr/local/ bin
