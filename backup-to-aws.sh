82cffcee6b989a98d2061f6aa831df6093f65067
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-02-02 10:07:13 -0500

    Update backup-to-aws.sh

--- backup-to-aws.sh
+++ backup-to-aws.sh
@@ -1,25 +1,11 @@
 #!/usr/bin/env bash
 
-unset AWS_ACCESS_KEY_ID
-unset AWS_SECRET_ACCESS_KEY
-file="/path/to/accessKeys.csv" # CHANGE WITH ACCESS KEYS
-cnt=0
- export AWS_DEFAULT_REGION=us-east-1
- export AWS_DEFAULT_OUTPUT=text
-while IFS=, read var1 var2  
-do 
-
-      if [ $cnt = 1 ]
-      then 
-        echo $cnt
-        export AWS_ACCESS_KEY_ID=$var1
-        export AWS_SECRET_ACCESS_KEY=$var2
-      fi
-
-      cnt=$(($cnt + 1))
-
-done < <(tr -d '\r' < "${file}" )
+# Remember to configure AWS CLI
 
 mkdir -p -- "$script-backups"
 
 tar -zcf usr/local/bin/script-backups/script-backup-$(date +%Y%m%d).tar.gz -C usr/local/ bin
+
+aws s3api put-object --bucket vig-script-backups --key script-backup-$(date +%Y%m%d).tar.gz --body script-backup-$(date +%Y%m%d).tar.gz
+
+# Crontab syntax: 0 0 */10 * * /usr/bin/env bash ./backup-to-aws >/dev/null 2>&1
