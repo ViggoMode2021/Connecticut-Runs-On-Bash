a1773d3bfedc6ed75673313515458be778836249
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-02-02 19:35:30 -0500

    Update backup-to-aws.sh

--- backup-to-aws.sh
+++ backup-to-aws.sh
@@ -2,10 +2,8 @@
 
 # Remember to configure AWS CLI
 
-mkdir -p -- "$script-backups"
+sudo tar -zcf /usr/local/bin/$(date +%Y%m%d).tar.gz -C /usr/local/ bin
 
-tar -zcf usr/local/bin/script-backups/script-backup-$(date +%Y%m%d).tar.gz -C usr/local/ bin
-
-aws s3api put-object --bucket vig-script-backups --key script-backup-$(date +%Y%m%d).tar.gz --body script-backup-$(date +%Y%m%d).tar.gz
+aws s3api put-object --bucket vig-script-backups --key script-backup-$(date +%Y%m%d).tar.gz --body /usr/local/bin/$(date +%Y%m%d).tar.gz
 
 # Crontab syntax: 0 0 */10 * * /usr/bin/env bash ./backup-to-aws >/dev/null 2>&1
