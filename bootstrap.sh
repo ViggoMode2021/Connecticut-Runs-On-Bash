1deca489ba205e9a11796bdd92e4af70ac4704cb
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-02-05 13:47:32 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,4 @@
-  GNU nano 6.2                                                                               setup.sh *                                                                                       
-sudo true
-
-# Install kernel extra's to enable docker aufs support
-# sudo apt-get -y install linux-image-extra-$(uname -r)
-
-# Add Docker PPA and install latest version
-# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-# sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-# sudo apt-get update
-# sudo apt-get install lxc-docker -y
-
+  GNU nano 6.2                                                                               setup.sh                                                                                         
 # Alternatively you can use the official docker install script
 wget -qO- https://get.docker.com/ | sh
 
@@ -30,10 +19,10 @@ sudo apt install git
 
 Sleep 5
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
 mkdir docker-database-success-academy
 cd docker-database-success-academy
 
 git clone https://github.com/jdaarevalo/docker_postgres_with_data.git
+
+cd docker_postgres_with_data
+sudo docker-compose up -d
