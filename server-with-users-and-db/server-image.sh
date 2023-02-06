77d2ededcaf5614be2c8644d5cbd2fd81264bde3
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-02-06 18:59:25 -0500

    Update server-image.sh

--- server-with-users-and-db/server-image.sh
+++ server-with-users-and-db/server-image.sh
@@ -7,17 +7,7 @@ install-bash-and-docker () {
   wget -qO- https://get.docker.com/ | sh
 
   # Install docker-compose
-  COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
-  sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-  sudo chmod +x /usr/local/bin/docker-compose
-  sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
-
-  # Install docker-cleanup command
-  cd /tmp
-  git clone https://gist.github.com/76b450a0c986e576e98b.git
-  cd 76b450a0c986e576e98b
-  sudo mv docker-cleanup /usr/local/bin/docker-cleanup
-  sudo chmod +x /usr/local/bin/docker-cleanup
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
  
 }
 
