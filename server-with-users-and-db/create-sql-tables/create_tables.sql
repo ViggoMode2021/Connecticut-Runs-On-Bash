33306c4d4cd55f1ab9b1b8fc4bbd332ba3aeadac
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-02-06 10:40:39 -0500

    Create create_tables.sql

---
+++ server-with-users-and-db/create-sql-tables/create_tables.sql
@@ -0,0 +1,18 @@
+-- Creation of schools table
+CREATE TABLE IF NOT EXISTS schools (
+  school_id SERIAL NOT NULL,
+  school_city VARCHAR NOT NULL,
+  school_name varchar(50) NOT NULL,
+  school_address VARCHAR(120),
+  PRIMARY KEY (school_id)
+);
+
+-- Creation of teachers table
+CREATE TABLE IF NOT EXISTS teachers (
+  teacher_id SERIAL NOT NULL,
+  teacher_name varchar(450) NOT NULL,
+  PRIMARY KEY (teacher_id)
+  CONSTRAINT fk_school
+      FOREIGN KEY(school_id) 
+	  REFERENCES schools(school_id)
+);
