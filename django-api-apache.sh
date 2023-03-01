5cea39ce438282ce39e7564b724aa46c92df4eda
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-03-01 18:55:46 -0500

    Update django-api-apache.sh

--- django-api-apache.sh
+++ django-api-apache.sh
@@ -2,31 +2,31 @@
 
 sudo apt-get update
 sudo apt install git
-yes| sudo add-apt-repository universe
+yes | sudo add-apt-repository universe # ‘Universe‘ is one of the standard repositories in Ubuntu,
+# which contains community maintained free and open-source software,
+# as opposed to ‘Main‘, which contains free and open-source software maintained by Ubuntu’s parent company Canonical.
 
-yes | sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3
+yes | sudo apt-get install python3-pip apache2 libapache2-mod-wsgi-py3 # Apache 2 and Python module
 
-sudo pip install virtualenv
+yes | sudo apt install python3-virtualenv # Install virtual environment
 
-git clone https://github.com/ViggoMode2021/turkish-api.git
+git clone https://github.com/ViggoMode2021/turkish-api.git # Clone Django-API repository
 
-cd turkish-api
+cd turkish-api # Change to directory
 
-virtualenv myprojectenv
+virtualenv myprojectenv # Create virtual environment
 
-source myprojectenv/bin/activate
+source myprojectenv/bin/activate # Activate virtual environment
 
-pip install django
-pip install djangorestframework
-pip install django-extensions
+pip install django # Install Django
+pip install djangorestframework # Install REST framework
+pip install django-extensions # Install Django-Extensions
 
-python3 manage.py migrate
+python3 manage.py migrate # Run initial migrations
 
-python3 manage.py runscript load
+python3 manage.py runscript load # Load data from csv information into database
 
-python3 manage.py migrate
-
-sudo chmod 777 /etc/apache2/sites-available
+sudo chmod 777 /etc/apache2/sites-available # Make sites available read write accessible
 
 sudo cat > /etc/apache2/sites-available/djangoproject.conf << EOF
 <VirtualHost *:80>
@@ -44,15 +44,18 @@ sudo cat > /etc/apache2/sites-available/djangoproject.conf << EOF
 
 EOF
 
-cd /home/ubuntu/turkish-api
+sudo chmod 444 /etc/apache2/sites-available # Make sites available read accessible only
 
-sudo sed -i "s/DEBUG = True/DEBUG = False/" settings.py
-testp=$(curl http://checkip.amazonaws.com)
-echo "$testp"
-sed -i "28s/\[\]/\['$testp']/" settings.py
+cd words
 
-sudo apt-get install tmux
+sudo sed -i "s/DEBUG = True/DEBUG = False/" settings.py # Change DEBUG to False with Sed in settings.py
+testp=$(curl http://checkip.amazonaws.com) # Curl server public IP and save to testp variable
+sed -i "28s/\[\]/\['$testp']/" settings.py # Add server public IP to ALLOWED_HOSTS in settings.py
 
-tmux new -s django-api
+sudo apt-get install tmux # Install terminal multiplexer
 
-python3 manage.py runserver 0.0.0.0:8000
+cd .. # Cd into previous directory
+
+tmux new -s django-api # Create new tmux session
+
+python3 manage.py runserver 0.0.0.0:8000 # Run Django app
